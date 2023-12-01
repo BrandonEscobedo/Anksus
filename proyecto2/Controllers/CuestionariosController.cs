4fb65f4e0fbd2f91d0f3892b7652eee69f36b148
sparss <76233826+sparss@users.noreply.github.com>
2023-11-30 20:49:06 -0600

    Se agregaron validaciones en CRUD cuestionarios

--- proyecto2/Controllers/CuestionariosController.cs
+++ proyecto2/Controllers/CuestionariosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using proyecto2.Models.dbModels;
 
 namespace proyecto2.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class CuestionariosController : Controller
     {
         private readonly ansksusContext _context;
@@ -18,7 +20,7 @@ namespace proyecto2.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         // GET: Cuestionarios
         public async Task<IActionResult> Index()
         {
